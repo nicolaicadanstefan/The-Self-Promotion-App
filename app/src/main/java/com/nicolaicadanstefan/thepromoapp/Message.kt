a1d551dbe71e2b90f23a3d63e0af8c4ffd950548
Nicolaica Stefan <snicolaica2@gmail.com>
2022-10-21 10:30:46 +0300

    The Self Promoting app is finished!

---
+++ app/src/main/java/com/nicolaicadanstefan/thepromoapp/Message.kt
@@ -0,0 +1,17 @@
+package com.nicolaicadanstefan.thepromoapp
+
+import java.io.Serializable
+
+data class Message(
+    val contactName: String,
+    val contactNumber: String,
+    val myDisplayName: String,
+    val includeJunior: Boolean,
+    val jobTitle: String?,
+    val immediateStart: Boolean,
+    val startDate: String?
+) : Serializable {
+    fun getFullJobDescription(): String = if (includeJunior) "a Junior $jobTitle" else "an $jobTitle"
+
+    fun getAvailability() = if (immediateStart) "immediately" else "from $startDate"
+}
